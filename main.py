c44db3a865749b5d9b9fb6b5ff8c3d780cb15997
walso1989 <Walso123.0>
2022-06-16 17:13:11 +0200

    Dumping VLAN Info to CVS. To be tested bug fix on CSV file

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import meraki
 import pandas
 import os
 
-
-# import logging
-
 def get_organizationid(API_KEY, org_name):
     # get organization_ID
     dashboard = meraki.DashboardAPI(API_KEY)
@@ -33,12 +30,17 @@ def get_networksid(API_KEY, organization_id):
 
 
 def main():
-    API_KEY = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
-    org_name = "devnet"
-    vlan_list = {}
-    cont = 0
+    #Variable definitions
+    API_KEY = "1f02da6afc079e16960440836c0a0e522f776d80"
+    org_name = "FIFCO"
+    current_path = os.getcwd()
+    csv_file = current_path + "//VLAN-Info.csv"
+
+    #getting organization ID and Networks within the organization
     organization_id = get_organizationid(API_KEY, org_name)
     network_id = get_networksid(API_KEY, organization_id)
+
+    #to extract VLAN information from the different networks in the organization
     for network in network_id:
         checking_network = network["id"]
         print(f" Extracting list of VLANs for network ID: {checking_network}")
@@ -46,48 +48,17 @@ def main():
         try:
             response = dashboard.appliance.getNetworkApplianceVlans(checking_network)
             print(f"{json.dumps(response, indent=2)}")
-            vlan_list[cont] = response
-            ++cont
+            try:
+                df = pandas.DataFrame(response)
+                df.to_csv(csv_file,index=True,sep=',',mode='a')
+            except csv as strerror:
+                logger.debug("Error creating File %s " + strerror)
+
         except meraki.APIError as e:
             print(f"Meraki API error: {e}")
             print(f"status code = {e.status}")
             print(f"reason = {e.reason}")
             print(f"error = {e.message}")
             continue
-
-    new_file_path = open('test9.csv', 'w')
-    m = csv.writer(new_file_path)
-    for vlan in vlan_list:
-        m.writerow(vlan_list.values())
-    new_file_path.close()
-
-
-    current_path = os.getcwd()
-    csv_file = current_path + "//VLAN-Info.csv"
-
-    final_df = pandas.DataFrame(vlan_list)
-
-    for id in range(0, len(vlan_list)):
-        df = pandas.DataFrame.from_dict(vlan_list[id])
-        final_df = pandas.concat([final_df, df], axis=0)
-
-    print(final_df)
-
-    final_df.to_csv(csv_file, sep=",",header=True,index=True)
-
 if __name__ == "__main__":
-    # Creating and setting logger
-    # logging.basicConfig(level=logging.INFO)
-    # logger = logging.getLogger(__name__)
-    #
-    # handler = logging.FileHandler("process.log")
-    # handler.setLevel(logging.INFO)
-    #
-    # # Create a logging format
-    # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    # handler.setFormatter(formatter)
-    #
-    # # Add the handlers to the logger
-    # logger.addHandler(handler)
-
     main()
